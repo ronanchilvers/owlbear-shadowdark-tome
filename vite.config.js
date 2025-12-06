8384fe90cd9fdf12c7ff34f629c441dc483118e9
Ronan Chilvers <ronan@d3r.com>
2025-12-06 12:33:28 +0000

    Commit dist and try to get it working!

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +9,14 @@ export default defineConfig({
       origin: "https://www.owlbear.rodeo",
     },
   },
+  plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: path.resolve(__dirname, './manifest.json'),
+            dest: './',
+          },
+        ],
+      }),
+    ]
 });
